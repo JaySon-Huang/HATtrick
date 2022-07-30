a6a322e12a7f06bab5266838e984db6363ad6144
JaySon-Huang <tshent@qq.com>
2022-07-30 08:02:46 +0800

    fix run selected benchmark
    Signed-off-by: JaySon-Huang <tshent@qq.com>

--- src/HATtrickBench.cpp
+++ src/HATtrickBench.cpp
@@ -108,17 +108,19 @@ int main(int argc, char* argv[]){
     }
 
     else if(UserInput::getWork() == 4){         // User selected to run the benchmark
-        auto startTime = chrono::system_clock::to_time_t(chrono::system_clock::now());
         int ac = UserInput::getAnalClients();
         int tc = UserInput::getTranClients();
+        Frontier::createFreshnessTable(tc); // the freshness table is recreated every time the number of T clients changes
+
+        auto startTime = chrono::system_clock::to_time_t(chrono::system_clock::now());
         cout << "\nChoice: [4] Run Benchmark with tc=" << tc << ", ac=" << ac << endl;
         cout << "START TIME of [4] " << ctime(&startTime) << endl;
         SQLHENV env = 0;
         Driver::setEnv(env);
         auto g = std::make_unique<Globals>();
         GetFromDB::getNumOrders(reinterpret_cast<int &>(g->loOrderKey), env);
-        g->barrierW = new Barrier(UserInput::getTranClients()+UserInput::getAnalClients());
-        g->barrierT = new Barrier(UserInput::getTranClients()+UserInput::getAnalClients());
+        g->barrierW = new Barrier(tc+ac);
+        g->barrierT = new Barrier(tc+ac);
         g->typeOfRun = none;
         auto workload = std::make_unique<Workload>();
         workload->ExecuteWorkloads(g.get());
@@ -129,6 +131,8 @@ int main(int argc, char* argv[]){
              << ", a_throughput=" << r->getAnalyticalThroughput() << endl;
         cout << "START TIME of [4] " << ctime(&startTime) << endl;
         cout << "END TIME of [4] " << ctime(&endTime) << endl;
+
+        Frontier::deleteTuples();
     }
     return 0;
 
